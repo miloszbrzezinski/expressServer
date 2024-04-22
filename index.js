0a6a3305f05635969372f263fd0d275639c8ffb2
miloszbrzezinski <109414155+miloszbrzezinski@users.noreply.github.com>
2024-04-22 23:50:10 +0200

    form added to the index.html

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import bodyParser from "body-parser"
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
@@ -8,8 +9,14 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({extended: true}))
+
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html")
+})
+
+app.post("/submit", (req, res) => {
+    console.log(req.body)
 })
 
 app.get("/about", (req, res) => {
