8827149ed556081ea6060edd2f29cdae1f8545f5
miloszbrzezinski <109414155+miloszbrzezinski@users.noreply.github.com>
2024-04-23 00:47:56 +0200

    custom middleware added

--- index.js
+++ index.js
@@ -9,17 +9,41 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 
 const app = express();
 const port = 3000;
+let bandName = "";
+
 
 app.use(bodyParser.urlencoded({extended: true}))
-app.use(morgan("tiny"));
+
+const bandNameGenerator = (req, res, next) => {
+    console.log(req.body);
+    bandName = req.body["street"] + req.body["pet"]
+    next();
+}
+
+app.use(bandNameGenerator)
+
+// const logger = (req, res, next) => {
+//     console.log("Request Method: ", req.method);
+//     console.log("Request URL: ", req.url);
+//     next();
+// }
+
+// app.use(logger);
+
+
+// app.use(morgan("tiny"));
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html")
 })
 
 app.post("/submit", (req, res) => {
-    console.log(req.body)
+    res.send(`<h1>Your band name is:</h1><h2>${bandName}</h2>`)
 })
+
+// app.post("/submit", (req, res) => {
+//     console.log(req.body)
+// })
 
 app.get("/about", (req, res) => {
     res.send("<h1>about</h1>")
